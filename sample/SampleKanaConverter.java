7f4880be4abf23b3a5582b018ccec2fe486bb1f7
saurabh <43170074+saurabhacellere@users.noreply.github.com>
2023-09-08 16:01:15 +0530

    Update SampleKanaConverter.java

--- sample/SampleKanaConverter.java
+++ sample/SampleKanaConverter.java
@@ -7,7 +7,7 @@ class SampleKanaConverter
         // Check for arg input
         if(args.length < 1) {
             System.out.println("Must specify a term to convert");
-            //System.out.println("EXAMPLE:");
+            System.out.println("EXAMPLE:");
             System.out.println("  java SampleKanaConverter ﾃｽﾄ");
             return;
         }
