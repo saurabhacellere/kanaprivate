14f6a70035e90396e6f31a1e84ce741a8a2433d4
saurabh <43170074+saurabhacellere@users.noreply.github.com>
2023-12-15 15:26:50 +0530

    Update SampleTemplate_202312_14748.java

--- src/main/java/com/mariten/kanatools/SampleTemplate_202312_14748.java
+++ src/main/java/com/mariten/kanatools/SampleTemplate_202312_14748.java
@@ -6,10 +6,12 @@ package com.mariten.kanatools;
 public class SampleTemplate_202312_14748
 {
   public static void addTest_SampleTemplate_202312_14748(char eval_char){
+         // System.out.println(" Added code issue ");
+         // System.out.println(" Added code issue ");
           System.out.println(" Added code issue ");
           System.out.println(" Added code issue ");
           System.out.println(" Added code issue ");
-          System.out.println(" Added code issue ");
-          System.out.println(" Added code issue ");
+   int i=0;
+    System.out.println(" Added code issue ");
 } 
  }
